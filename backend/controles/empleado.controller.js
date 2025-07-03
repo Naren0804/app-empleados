2dc8022f58fcae74df84610ead0a82e77b804391
Naren0804 <narenxd123@gmail.com>
2025-07-03 13:25:06 -0500

    Primer commit

---
+++ backend/controles/empleado.controller.js
@@ -0,0 +1,54 @@
+/**
+ * Se coloca el controlador como un objeto y luego se exporta como
+ * se requiere primero el modelo empleado
+ */
+
+ const Empleado = require('../modelos/empleado');
+ const empleadoCtrl = {};
+ 
+ /**
+  * DEFINO LOS MÉTODOS  */
+ 
+ //Obtener todos los empleados
+ empleadoCtrl.getEmpleados = async (req, res) => {
+     const empleados = await Empleado.find();
+     res.json(empleados);   
+}                    
+ 
+ // Crear empleados
+ 
+ empleadoCtrl.createEmpleados = async (req, res) => { 
+    const empleado = new Empleado(req.body);
+    await empleado.save();    
+    res.json({
+        'status': 'Empleado guardado'
+    });
+ }
+
+ //Conseguir un único empleado 
+ empleadoCtrl.getUnicoEmpleado = async (req, res) => {     
+     const empleadoUnico = await Empleado.findById(req.params.id); 
+     res.json(empleadoUnico);
+ }
+ 
+ //Actualizar empleado
+ empleadoCtrl.editarEmpleado = async (req, res) =>  {
+     const { id } = req.params; 
+     const emepleadoEdit = {  
+         nombre: req.body.nambre,
+         cargo: req.body.sucursal,
+         sucursal: req.body.cargo,
+         salario: req.body.salario
+     };
+     await Empleado.findByIdAndUpdate(id, {$set: emepleadoEdit}, {new:  true}); 
+     res.json({status: 'Empleado Actualizado'});
+ }
+
+ // Eliminar empleado 
+ empleadoCtrl.eliminarEmpleado = async (req, res) => {
+     await Empleado.findByIdAndDelete(req.params.id);
+     res.json({status: 'Empleado Eliminado'});
+}
+
+// Exportar el modulo empleado
+module.exports = empleadoCtrl;
